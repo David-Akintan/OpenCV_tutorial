30f169fcd366b1ad78297c5464108f710e859cf2
Dayve <null@gmail.com>
2022-03-28 01:31:32 +0100

    Loading an image and processing it
    This program shows how to load an image file using the OPenCv module.
    Also, there are processes carried out on the image such as rotation and
    resizing.
    Finally, the processed image is stored as a file.

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-import cv2 
+# import the necessary modules
 
-image = cv2.imread('assets/12.jpg')
+import cv2 as cv
+import sys 
 
-print(image.shape)
+image = cv.imread('assets/12.jpeg', 1)   # reads the image 
+# Check if the image loaded successfully
+if image is None:
+    sys.exit('Error while loading image')
+
+image = cv.resize(image, (0, 0), fx=0.5, fy=0.5)  # resize the image (Half its original)
+image = cv.rotate(image, cv.cv2.ROTATE_90_CLOCKWISE)  # rotate the image
+
+cv.imshow("Display Window", image)  # Displays image on a new window
+cv.waitKey(0)
+cv.destroyAllWindows()
+
+cv.imwrite('assets/new_img.jpg', image)   # Writes the processed image to a new file
